ae353155de9b268bca3a6ca283f1ff2ff23e15fa
Judith Yusuf <judithyusuf00@gmail.com>
2024-04-24 23:07:27 +0100

    mm

--- src/api/rovers/route.jsx
+++ src/api/rovers/route.jsx
@@ -1,5 +1,5 @@
 export default async function fetchManifest(rover) {
-    const apiKey = `${ process.env.NASA_API_KEY}`
+    const apiKey = `${process.env.NASA_API_KEY}`
 
     const result = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/` + rover + `/?api_key=` + apiKey)
 
