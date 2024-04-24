8c91a13e7aa0849d55c932017d997c6390f8c9b4
Judith Yusuf <judithyusuf00@gmail.com>
2024-04-25 00:17:47 +0100

    Trying again

--- src/api/photos/route.jsx
+++ src/api/photos/route.jsx
@@ -1,10 +1,9 @@
 export default async function fetchQuery(x, y) {
     
+    const apiKey = process.env.NEXT_PUBLIC_NASA_API
+    
     async function fetchPhotos(date, rover) {
-        const apiKey = process.env.NASA_API_KEY
-        // console.log(apiKey)
-        
-        const result = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/` + rover + `/photos?earth_date=` + date + `&api_key=` + apiKey)
+        const result = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/` + rover + `/photos?earth_date=` + date + `&api_key=${apiKey}`)
         
         if(!result.ok) {
             throw new Error('Failed to fetch data')
--- src/api/rovers/route.jsx
+++ src/api/rovers/route.jsx
@@ -1,5 +1,6 @@
 export default async function fetchManifest(rover) {
     const apiKey = process.env.NASA_API_KEY
+    console.log(apiKey)
 
     const result = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/` + rover + `/?api_key=` + apiKey)
 
@@ -8,6 +9,8 @@ export default async function fetchManifest(rover) {
     }
     
     const data =  await result.json()
+
+    console.log
     
     return data
 }
